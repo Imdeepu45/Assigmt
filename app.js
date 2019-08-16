9c8c3ec0b2750e59e5178a66951276ba1dd63b76
Dilee Deepu <dileepdeepu1995@yahoo.com>
2019-08-16 14:54:16 +0530

    https://github.com/Imdeepu45/Assigmt.git

---
+++ app.js
@@ -0,0 +1,25 @@
+const Express=require('express');
+
+
+ var app=new Express();
+
+
+ app.set('view engine','ejs');
+
+
+app.use(Express.static(__dirname+"/public"));
+
+ nav=[
+     {link:'/books',title:'Books'},
+     {link:'auther',title:'Authers'}
+ ];
+
+
+app.get("/",(req,res)=>{
+    res.render('index',nav);
+    
+});
+
+app.listen(3000,()=>{
+    console.log("work in Progress");
+});
